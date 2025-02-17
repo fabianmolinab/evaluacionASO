4eed0c872802ff355b4ccb44f958cc7d1dc78993
Molina-Bermudez Fabian-Andres <molinafabian11b@gmail.com>
2025-02-17 17:49:02 -0500

    feat: add facade imp

---
+++ src/main/java/org/example/facade/imp/ConvertidorTexto.java
@@ -0,0 +1,13 @@
+package org.example.facade.imp;
+
+import org.example.facade.FConvertidorTexto;
+import org.example.facade.dto.EntradaDtoFacade;
+import org.example.facade.dto.SalidaDtoFacade;
+
+public class ConvertidorTexto  implements FConvertidorTexto {
+
+    @Override
+    public SalidaDtoFacade convertirTexto(EntradaDtoFacade texto) {
+        return null;
+    }
+}
